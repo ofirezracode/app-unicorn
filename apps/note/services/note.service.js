f0944068aa4ae0a2ebb4a29010dc4b11340bc02b
ofirezracode <ofirezracode@gmail.com>
2023-05-10 15:49:27 +0300

    FIX: moved notesData to the service

--- apps/note/services/note.service.js
+++ apps/note/services/note.service.js
@@ -1,7 +1,36 @@
 import { utilService } from '../../../services/util.service.js'
 import { storageService } from '../../../services/storage.service.js'
 import { asyncStorageService } from '../../../services/async-storage.service.js'
-import { notesData } from './_notes.js'
+
+const notesData = [
+  {
+    id: 'n101',
+    createdAt: 1112222,
+    type: 'txt',
+    isPinned: true,
+    style: { backgroundColor: '#00d' },
+    info: { txt: 'Fullstack Me Baby!' },
+  },
+  {
+    id: 'n102',
+    type: 'img',
+    isPinned: false,
+    info: { url: 'http://some-img/me', title: 'Bobi and Me' },
+    style: { backgroundColor: '#00d' },
+  },
+  {
+    id: 'n103',
+    type: 'todos',
+    isPinned: false,
+    info: {
+      title: 'Get my stuff together',
+      todos: [
+        { txt: 'Driving license', doneAt: null },
+        { txt: 'Coding power', doneAt: 187111111 },
+      ],
+    },
+  },
+]
 
 const NOTE_KEY = 'noteDB'
 
