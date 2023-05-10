bd41debbb8d77a4b5edb3b15f41b32d7f5d9472b
ofirezracode <ofirezracode@gmail.com>
2023-05-10 20:49:00 +0300

    Delivery2 - Wednesday 21:00

--- views/home.jsx
+++ views/home.jsx
@@ -4,7 +4,7 @@ export function Home() {
       <div>
         <h1>App 🦄</h1>
         <p>Yalla balagan!</p>
-        <p>Delivery1 - Wednesday 21:00</p>
+        <p>Delivery2 - Wednesday 21:00</p>
         <a href="#">Test</a>
       </div>
       <div></div>
